b40d1e57832ba3a52eea37e2575645c00a78a6c9
unknown <ee06b056@gmail.com>
2018-06-18 23:32:34 -0400

    add template engine

--- M101JS/Week01/expres_hello_world/app.js
+++ M101JS/Week01/expres_hello_world/app.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const app = express();
+const engines = require('consolidate');
+
+app.engine('html',engines.nunjucks);
+app.set('view engine','html');
+app.set('views', __dirname+'/views');
 
 app.get('/', (req, res) => {
-    res.send('Hello world!');
+    res.render('hello',{name:'libo'});
 });
 
 let server = app.listen(3000, function () {
